efe6f7605e95d72fe327bd86b067cfeb54de7482
ArsalJan34 <arsaljaan532@gmail.com>
2025-11-01 00:13:44 +0500

    Changes made

--- script.js
+++ script.js
@@ -16,3 +16,12 @@ function hideSidebar() {
   const sidebar = document.querySelector(".mobile-nav");
   sidebar.classList.remove("show");
 }
+// function hideSidebar(){
+//   const sidebar = document.querySelector(".mobile-nav");
+//   sidebar.classList.add("show");
+// }
+// function hideSidebar() {
+//   const sidebar = document.querySelector(".mobile-nav");
+//   sidebar.classList.add("show");
+
+// }
